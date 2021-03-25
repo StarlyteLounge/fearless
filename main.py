2b8d1746331b4bc578f79a48834f705f62c9fb60
Jon McCarty <mccartyj@tena-sda.org>
2021-03-25 17:54:29 -0400

    added a way to add/update items. added a RESTful way to delete items

--- main.py
+++ main.py
@@ -3,19 +3,32 @@
 from flask import Flask
 from flask import request
 
+ppc = Flask(__name__)
 
-ppc = Flask(__name__)  # one server serves them all
-
-items = {'1': 'one', '4': 'four'}  # populated via API
+items = {}  # populated via API
 
 
 @ppc.route('/')
 def landing():
+    """A not-very-helpful landing page"""
     return "nothing to see here. Try /item"
 
 
-@ppc.route('/item', methods=['GET'])
+@ppc.route('/item', methods=['GET', 'POST', 'DELETE'])
 def item():
+    """Respond to the item REST/HTTP requests"""
+    if request.method == 'GET':
+        return get_method()
+    elif request.method == 'POST':
+        items[request.form['id']] = request.form['val']
+        return '200'
+    elif request.method == 'DELETE':  # NOTE that DELETE is an HTTP construct, and not supported by html
+        items.pop(request.form['idx'])
+        return '200'
+
+
+def get_method():
+    """Get all of the indicated items, or all of the items if there are no indicated items"""
     if request.args:
         return {i: items.get(i) for i in request.args.getlist('id')}
     else:
